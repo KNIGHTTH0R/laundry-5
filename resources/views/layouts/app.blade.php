4a7c1084b820be4133e6be4b9a380828bb712837
jiangxiaolhm <jiangxiao.lhm@gmail.com>
2017-09-25 18:43:30 +1000

    Change app background to yellow tutorial challenge

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body style="background: #ff0;">
         <div id="app">
             <nav class="navbar navbar-default navbar-static-top">
                 <div class="container">
