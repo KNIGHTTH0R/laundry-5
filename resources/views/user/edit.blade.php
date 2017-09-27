5d4117e7d240414542c132d133227f0c7eac0260
YZH <edyang19@gmail.com>
2017-09-27 23:21:21 +1000

    Complete the User - Edit function - YZH

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -24,12 +24,36 @@
                                 <td><input type="text" name="lastname" value="{{ $user->lastname}}"></td>
                             </tr>
                             <tr>
-                                <td>Postcode</td>
-                                <td><input type="text" name="postcode" value="{{ $user->postcode}}"></td>
+                                <td>Email</td>
+                                <td><input type="text" name="lastname" value="{{ $user->email}}"></td>
                             </tr>
                             <tr>
+                                <td>Phone Number</td>
+                                <td><input type="text" name="lastname" value="{{ $user->phone}}"></td>
+                            </tr>
+                            <tr>
+                                <td>Address 1</td>
+                                <td><input type="text" name="lastname" value="{{ $user->addressline1}}"></td>
+                            </tr>
+                            <tr>
+                                <td>Address 2</td>
+                                <td><input type="text" name="lastname" value="{{ $user->addressline2}}"></td>
+                            </tr>
+                             <tr>
                                 <td>Suburb</td>
                                 <td><input type="text" name="lastname" value="{{ $user->suburb}}"></td>
+                            </tr>
+                            <tr>
+                                <td>Sate</td>
+                                <td><input type="text" name="lastname" value="{{ $user->state}}"></td>
+                            </tr>
+                            <tr>
+                                <td>Postcode</td>
+                                <td><input type="text" name="postcode" value="{{ $user->postcode}}"></td>
+                            </tr>                           
+                            <tr>
+                                <td>Country</td>
+                                <td><input type="text" name="lastname" value="{{ $user->country}}"></td>
                             </tr>
                             <tr>
                                 <td></td>
