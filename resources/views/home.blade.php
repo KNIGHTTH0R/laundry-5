1fc458fdd5bd09246e6909a5132e4d07ea1e5c67
jiangxiaolhm <jiangxiao.lhm@gmail.com>
2017-10-16 05:11:21 +1100

    little fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,6 +70,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
             <div class="top-right links">
+                <a href="{{ url('about') }}">About Us</a>
                 @auth
                 @if (Auth::user()->role == 'staff')
                 <a href="{{ url('admin') }}">Admin Console</a>
